411091f261139cdee94f146377ca11afacc94cc7
Josh-yy <126300620+Josh-yy@users.noreply.github.com>
2025-08-31 10:25:08 +0800

    refactor: simplify HTML structure by requiring head partial

--- views/index.view.php
+++ views/index.view.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Anime Tracker</title>
-        <script src="https://cdn.tailwindcss.com"></script>
-        <link rel="stylesheet" href="css/home.css">
-</head>
+<?php require "partials/head.php" ?>
+
 <body>
-    <?php require('partials/navigation.php') ?>
+    <?php require 'partials/navigation.php' ?>
     <main>
 
     </main>
