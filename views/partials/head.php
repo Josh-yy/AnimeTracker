a10d160817fca6dad82cb4ba403158c8ae8f955f
Josh-yy <126300620+Josh-yy@users.noreply.github.com>
2025-09-02 10:34:18 +0800

    add jQuery script link in head.php

--- views/partials/head.php
+++ views/partials/head.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anime Tracker</title>
+
         <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
         <link rel="stylesheet" href="views/css/root.css">
         <link rel="stylesheet" href="views/css/home.css">
 
