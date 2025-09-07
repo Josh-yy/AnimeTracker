2d5ba9791db6b8967e85909e818bda2747432889
Josh-yy <126300620+Josh-yy@users.noreply.github.com>
2025-09-07 08:12:53 +0800

    feat: add routes for login, register, and logout in router.php

--- router.php
+++ router.php
@@ -5,6 +5,9 @@ $url = parse_url($_SERVER['REQUEST_URI'])['path'];
 $routes = [
     '/' => 'controllers/home/index.php',
     '/search' => 'controllers/home/search.php',
+    '/login' => 'controllers/auth/login.php',
+    '/register' => 'controllers/auth/register.php',
+    '/logout' => 'controllers/auth/logout.php',
 ];
 
 function routeToController($url, $routes){
