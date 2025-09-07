e4c0ab153627a096c29604a5aa1e1159cce4585d
Josh-yy <126300620+Josh-yy@users.noreply.github.com>
2025-09-07 08:21:29 +0800

    feat: add sign in container for interchangable content in login modal; add tailwind styling for better ui

--- views/modals/login.php
+++ views/modals/login.php
@@ -1,5 +1,6 @@
-<div id="modal-backdrop">
-    <div id="login-modal" class="flex bg-[var(--tint-background-color)] w-[60%] items-center p-[3em] rounded-lg shadow-lg shadow-[var(--primary-color)]">
+<div id="modal-backdrop" class="hidden">
+    <div id="login-modal" class="relative flex bg-[var(--tint-background-color)] w-[60%] items-center p-[3em] rounded-lg shadow-lg shadow-[var(--primary-color)]">
+        <button id="close-modal" class="absolute top-[1em] right-[1em]"><i style="color: var(--primary-color); font-size: 2.5em; font-weight: bold;" class='bx bx-x'></i></button>
         <div class="flex flex-col gap-[2em] w-[50%] ">
             <div class="flex justify-center">
                 <div class="w-[20%]">
@@ -17,18 +18,52 @@
                 <img src="/public/external/anime-bg.jpg" alt="anime background">
             </div>
         </div>
-        <div class="flex flex-col justify-center items-center px-[5em] gap-[1em] w-[50%]">
-            <h1 class="text-2xl">Sign up</h1>
-            <div class="w-full">
-                <label for="username">Username</label>
-                <input id="username" class="input-field" name="" type="text">
+
+        <div class="flex absolute w-[50%] right-0 justify-center items-center">
+            <div id="sign-in-modal" class="flex flex-col px-[5em] absolute dataMistShow">
+                <form id="sign-in-form" class="flex flex-col gap-[1em] ">
+                    <div class="flex flex-col justify-center relative mb-6">
+                        <img class="bocchi w-[20em] self-center object-cover" src="/public/external/bocchi.png" alt="bocchi bg">
+                        <h1 class="text-3xl font-bold absolute bottom-0 left-[35%] z-100 text-center text-shadow-2xl text-[var(--primary-color)] mt-[-1em]">Sign in</h1>
+                    </div>
+                    <div class="w-full">
+                        <label for="username">Username</label>
+                        <input id="username" class="input-field" name="" type="text">
+                        <p id="sign-in-error-message" class="text-sm font-bold text-red-500 mt-1"></p>
+                    </div>
+                    <div class="w-full">
+                        <label for="password">Password</label>
+                        <input id="password" class="input-field" name="" type="password">
+                    </div>
+                    <label class="self-start" for="remember-me"><input id="remember-me" type="checkbox">Remember me</label>
+                    <button id="sign-in-submit-button" type="button" class="primary-button w-full">Sign in</button>
+                    <p class="text-center">Don't have an account? <button type="button" id="sign-up-button-modal" class="text-[var(--primary-color)]">Sign up</button></p>
+                </form>
             </div>
-            <div class="w-full">
-                <label for="password">Password</label>
-                <input id="password" class="input-field" name="" type="password">
+
+            <div id="sign-up-modal" class="flex flex-col px-[5em] absolute dataMistHide">
+                <form id="sign-up-form" class="flex flex-col gap-[1em] ">
+                    <div class="flex flex-col justify-center relative mb-6">
+                        <img class="bocchi w-[20em] self-center object-cover" src="/public/external/one_piece.png" alt="one piece bg">
+                        <h1 class="text-3xl font-bold absolute bottom-0 left-[35%] z-100 text-center text-shadow-2xl text-[var(--primary-color)] mt-[-1em]">Sign up</h1>
+                    </div>
+                    <div class="w-full">
+                        <label for="username">Username</label>
+                        <input id="username" class="input-field" name="username" type="text" required>
+                        <p id="sign-up-error-message" class="text-sm font-bold text-red-500 mt-1"></p>
+                    </div>
+                    <div class="w-full">
+                        <label for="password">Password</label>
+                        <input id="password" class="input-field" name="password" type="password" required>
+                    </div>
+                    <div class="w-full">
+                        <label for="password_confirmation">Confirm Password</label>
+                        <input id="password_confirmation" class="input-field" name="password_confirmation" type="password" required>
+                    </div>
+                    <button id="sign-up-submit-button" type="button" class="primary-button w-full">Sign up</button>
+                    <p class="text-center">Already have an account? <button type="button" id="sign-in-button-modal" class="text-[var(--primary-color)]">Sign in</button></p>
+                </form>
             </div>
-            <label class="self-start" for="remember-me"><input id="remember-me" type="checkbox">Remember me</label>
-            <button class="primary-button w-full">Sign up</button>
         </div>
     </div>
 </div>
