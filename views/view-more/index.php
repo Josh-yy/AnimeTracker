8b88175f1189c8883f9c93244ed7dfd2ad5f77fd
Josh-yy <126300620+Josh-yy@users.noreply.github.com>
2025-09-09 17:08:38 +0800

    feat: create new page for view-more functionality

---
+++ views/view-more/index.php
@@ -0,0 +1,12 @@
+<?php require 'views/view-more/partials/head.php' ?>
+
+
+<body class="px-[5%] py-[1em] ">
+    <?php require 'views/partials/navigation.php' ?>
+    <main>
+        <?php require 'views/view-more/partials/heading.php' ?>
+        <?php require 'views/view-more/partials/images-section.php' ?>
+
+    </main>
+</body>
+</html>
