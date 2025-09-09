cfefc77a109cc5ae01760347981987df1656e547
Josh-yy <126300620+Josh-yy@users.noreply.github.com>
2025-09-09 17:03:20 +0800

    feat: create helper function to turn dynamic request ranking into readable heading

--- helpers/helper.php
+++ helpers/helper.php
@@ -26,18 +26,42 @@ function formatHumanText(string $string){
 }
 
 function formatHumanReadable(array $animes){
-    $animes = array_map(function($anime){
-    $rawDate = $anime['node']['start_date'] ?? null;
-    $rawDuration = $anime['node']['average_episode_duration'] ?? null;
-    $rawStatus = $anime['node']['status'] ?? null;
-    
-    $anime['node']['start_date'] = formatHumanDate($rawDate);
-    $anime['node']['average_episode_duration'] = formatHumanDuration($rawDuration);
-    $anime['node']['status'] = formatHumanText($rawStatus);
+    $animes = array_map(function($anime) {
+        $rawDate = $anime['node']['start_date'] ?? null;
+        $rawDuration = $anime['node']['average_episode_duration'] ?? null;
+        $rawStatus = $anime['node']['status'] ?? null;
+        
+        $anime['node']['start_date'] = formatHumanDate($rawDate);
+        $anime['node']['average_episode_duration'] = formatHumanDuration($rawDuration);
+        $anime['node']['status'] = formatHumanText($rawStatus);
 
-    return $anime;
+        return $anime;
 
-}, $animes);
+    }, $animes);
 
     return $animes;
+}
+
+function getRankingHeading($rankingType){
+    switch ($rankingType) {
+        case 'all':
+            return "Top Anime Series";
+        case 'airing':
+            return "Top Airing Anime";
+        case 'upcoming':
+            return "Top Upcoming Anime";
+        case 'tv':
+            return "Top Anime TV Series";
+        case 'ova':
+            return "Top Anime OVA Series";
+        case 'movie':
+            return "Top Anime Movies";
+        case 'special':
+            return "Top Anime Specials";
+        case 'bypopularity':
+            return "Top Anime by Popularity";
+        case 'favorite':
+            return "Top Favorited Anime";
+
+    }
 }
